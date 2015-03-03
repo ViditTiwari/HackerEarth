5efdf9bafd8ee55503ed5fc287910b5e8d8fee0f
Vidit Tiwari <vidittiwari95@gmail.com>
2015-03-04 01:25:54 +0530

    sample-code

---
+++ sample.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<math.h>
+
+using namespace std;
+
+int func(int n){
+
+    return 1;
+}
+
+int main()
+ {
+
+   int total,cas;
+   cin >>total;
+
+  for(int i=0;i<total;i++)
+
+  { cin>>cas;
+     func();
+  }
+  return 0;
+
+ }
